4b24a4ffaba33b943bc6efbd69ed87245852bcb9
dzlatarov <danzlatarov1@gmail.com>
2021-01-28 20:05:50 +0200

    Add Polindrom checking algorithm

---
+++ Algorithms/IsPalindrom/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IsPalindrom
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var word = "rotor";
+            var isPolindrom = IsPalindrom(word);
+            Console.WriteLine(isPolindrom);
+        }
+
+        private static bool IsPalindrom(string word)
+        {
+            if (word.Length <= 1)
+            {
+                return true;
+            }
+            if (word[0] != word[word.Length - 1])
+            {
+                return false;
+            }
+            return IsPalindrom(word.Substring(1, word.Length - 2));
+        }
+    }
+}
